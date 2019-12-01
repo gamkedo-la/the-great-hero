8851768fb6d48e2433c27813c44d3eac3ab771d4
Justin <jchingame@gmail.com>
2019-12-01 01:16:31 -0800

    Added in generic bat placeholders, added cannon placeholder, spawning of bullets, bullets, created generic ending for game, and placeholder tilemaps for grass and sea, added in potatoes too

---
+++ Assets/Scripts/BulletMoving.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMoving : MonoBehaviour
+{
+    public GameObject bullet;
+    public Rigidbody rb;
+    public float bulletSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = this.gameObject;
+        rb = bullet.GetComponent<Rigidbody>();
+
+        rb.AddForce(transform.forward * bulletSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public GameObject bulletSpawn;
+    public Vector3 bulletSpawnLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletSpawnLocation = bulletSpawn.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletSpawnLocation = bulletSpawn.transform.position;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, bulletSpawnLocation, Quaternion.identity);
+        }
+    }
+}
