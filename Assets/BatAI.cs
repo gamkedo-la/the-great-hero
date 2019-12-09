378900212799cc000a9d465a92bb9208c143f192
Justin Chin <jchingames@gmail.com>
2019-12-09 14:53:37 -0800

    Bat AI, bat moves to cart, hovers then retreats

--- Assets/BatAI.cs
+++ Assets/BatAI.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class BatAI : MonoBehaviour
 {
     public float batSpeed = 5f;
+    public float batHoverAmt = 0.3f;
+    public float batHoverSpeed = 4.0f;
+    public float batHoverDist = 3.0f;
+    public float batStealTime = 2.0f;
+
+    //Doing State with Primitives First, could refactor to Enumerators later
+    private const int B_State_Approach = 0;
+    private const int B_State_Stealing = 1;
+    private const int B_State_Retreat = 2;
+    private int stateNow = B_State_Approach;
+    private Vector3 stealPos = Vector3.zero;
+    private float stealStartTime = 0.0f;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +28,36 @@ public class BatAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(FromAnywhereSingleton.instance.transform.position);
+        if(stateNow == B_State_Approach)
+        {
+            transform.LookAt(FromAnywhereSingleton.instance.transform.position);
 
-        transform.position += transform.forward * Time.deltaTime * batSpeed;
-        //FromAnywhereSingleton.instance.transform.position 
+            transform.position += transform.forward * Time.deltaTime * batSpeed;
+
+            if(Vector3.Distance(transform.position, FromAnywhereSingleton.instance.transform.position) < batHoverDist)
+            {
+                stateNow++;
+                stealPos = transform.position;
+
+                stealStartTime = Time.timeSinceLevelLoad;
+            }
+        }
+        else if(stateNow == B_State_Stealing)
+        {
+            float timeStealing = Time.timeSinceLevelLoad - stealStartTime;
+            transform.position = stealPos + batHoverAmt * Vector3.up * Mathf.Cos(timeStealing * batHoverSpeed);
+
+            if(timeStealing > batStealTime)
+            {
+                stateNow++;
+            }
+        }
+        else if(stateNow == B_State_Retreat)
+        {
+            transform.LookAt(FromAnywhereSingleton.instance.transform.position);
+
+            transform.position -= transform.forward * Time.deltaTime * batSpeed;
+        }
+
     }
 }
