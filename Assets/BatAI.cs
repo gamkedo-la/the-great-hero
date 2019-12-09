ac0caf06b727533da91bd9eeb6adef4d645b6d69
Justin Chin <jchingames@gmail.com>
2019-12-09 14:26:44 -0800

    Made Bat AI Move Forwards and Face the Direction of the Cart

---
+++ Assets/BatAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatAI : MonoBehaviour
+{
+    public float batSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(FromAnywhereSingleton.instance.transform.position);
+
+        transform.position += transform.forward * Time.deltaTime * batSpeed;
+        //FromAnywhereSingleton.instance.transform.position 
+    }
+}
