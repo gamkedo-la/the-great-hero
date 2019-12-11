ccb53d8119c13853e04ff1f8b0a760264670a534
Karel Terra <karel.terra@gmail.com>
2019-12-10 21:36:19 -0500

    Robo car wip

---
+++ Assets/Scripts/RoboCarSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboCarSpawner : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject roboCarPrefab;
+	[SerializeField]
+	private Transform spawnLocation;
+
+	[SerializeField]
+	[Tooltip("Spawned every x serconds")]
+	private float spawnRate;
+	
+
+	[SerializeField]
+	private Transform potatoHolderTransform;
+
+	private float timer = 0f;
+
+
+	private void Update()
+	{
+		timer += Time.deltaTime;
+
+		if(timer > spawnRate)
+		{
+			//Spawn Robo Car
+
+			timer = timer - spawnRate;
+		}
+	}
+
+}
