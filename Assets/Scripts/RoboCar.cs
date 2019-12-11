aaf70fe382e3bf78fe109f5d3817a912309dcec4
Karel Terra <karel.terra@gmail.com>
2019-12-10 22:39:03 -0500

    RoboCar Spawner, Car's move to train

---
+++ Assets/Scripts/RoboCar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboCar : MonoBehaviour
+{
+
+	[SerializeField]
+	private Transform armPivotTransform;
+
+	[SerializeField]
+	private float moveSpeed;
+
+	private Vector3 startPosition;
+	private Vector3 targetPosition;
+
+	private bool initialized;
+
+	public void Initialize(Vector3 targetPosition)
+	{
+		startPosition = transform.position;
+		this.targetPosition = targetPosition;
+
+		initialized = true;
+	}
+
+
+	private void Update()
+	{
+		if(initialized)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+		}
+	}
+
+}
--- Assets/Scripts/RoboCarSpawner.cs
+++ Assets/Scripts/RoboCarSpawner.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class RoboCarSpawner : MonoBehaviour
 {
+	private readonly float TARGET_Z_POS = 69.149f;
+
+
 	[SerializeField]
 	private GameObject roboCarPrefab;
 	[SerializeField]
-	private Transform spawnLocation;
+	private Transform spawnLocationMin;
+	[SerializeField]
+	private Transform spwnLocationMax;
 
 	[SerializeField]
 	[Tooltip("Spawned every x serconds")]
@@ -19,17 +24,52 @@ public class RoboCarSpawner : MonoBehaviour
 
 	private float timer = 0f;
 
+	private ObjectPool<RoboCar> roboCarPool;
+
+	private void Start()
+	{
+		//Spawn at the start
+		timer = spawnRate;
+
+		roboCarPool = new ObjectPool<RoboCar>();
+		roboCarPool.InitializeObjectPool(roboCarPrefab, 20, transform);
+	}
 
 	private void Update()
 	{
-		timer += Time.deltaTime;
-
-		if(timer > spawnRate)
+		if(timer >= spawnRate)
 		{
 			//Spawn Robo Car
+			SpawnRoboCar();
 
 			timer = timer - spawnRate;
 		}
+
+		timer += Time.deltaTime;
+	}
+
+	private void SpawnRoboCar()
+	{
+		RoboCar roboCar = roboCarPool.GetObject();
+
+		Vector3 spawnLocation = GetSpawnLocation();
+		roboCar.transform.position = spawnLocation;
+
+		spawnLocation.z = TARGET_Z_POS;
+		roboCar.Initialize(spawnLocation);
+
+		roboCar.gameObject.SetActive(true);
+	}
+
+	private Vector3 GetSpawnLocation()
+	{
+		Vector3 spawnLocation = Vector3.zero;
+
+		spawnLocation.x = Random.Range(spawnLocationMin.position.x, spwnLocationMax.position.x);
+		spawnLocation.y = Random.Range(spawnLocationMin.position.y, spwnLocationMax.position.y);
+		spawnLocation.z = Random.Range(spawnLocationMin.position.z, spwnLocationMax.position.z);
+
+		return spawnLocation;
 	}
 
 }
