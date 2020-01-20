40e623b69b915b4bc82d57d4fd046e770c5a941e
Gamkedo <chris@gamkedo.com>
2020-01-19 22:28:03 -0600

    Cursor lock for in-browser play, camera tilt limit added to prevent flipping upsidedown over top or bottom from accumulated mouse offset

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -10,10 +10,13 @@ public class CameraScript : MonoBehaviour
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
-    // Start is called before the first frame update
+
+    private float TILT_RANGE = 65.0f;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -22,11 +25,19 @@ public class CameraScript : MonoBehaviour
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
 
+        pitch = Mathf.Clamp(pitch, -TILT_RANGE, TILT_RANGE);
+
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 
         if(Input.GetKeyDown(KeyCode.M))
         {
             ToggleSound();
+        }
+
+        if(Input.GetButtonDown("Fire1")) // relock on next fire
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
     }
 
